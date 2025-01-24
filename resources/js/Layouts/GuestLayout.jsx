9917c0b79eb31c95a0778abf2d6d8d1ee5196caa
ohmra <rantohary@gmail.com>
2025-01-24 09:11:56 +0300

    added bg and logo

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,16 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
+import authBg from '../../../public/images/auth-bg.jpg';
 
 export default function Guest({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+        <div className="flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0"
+             style={{ backgroundImage: `url(${authBg})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',}}>
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <img src="./images/Logo.png" alt="pizza logos" className={"w-64 rounded-lg"}/>
                 </Link>
             </div>
 
