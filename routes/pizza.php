d712ce34be010520585ca4a479c2ee580ea64bcb
ohmra <rantohary@gmail.com>
2025-01-24 11:06:17 +0300

    remove unecessary routes

--- routes/pizza.php
+++ routes/pizza.php
@@ -5,10 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('pizzas', [PizzaController::class, 'index'])->name('pizzas')->middleware('auth');
 
 Route::middleware('is_admin')->group(function(){
-    Route::get('pizza/create', [PizzaController::class, 'create'])->name('pizza.create');
     Route::post('pizza/create', [PizzaController::class, 'store']);
     Route::delete('pizza/delete/{id}', [PizzaController::class, 'destroy'])->name('pizza.delete');
-    Route::get('pizza/edit/{id}', [PizzaController::class, 'edit'])->name('pizza.edit');
     Route::put('pizza/edit/{id}', [PizzaController::class, 'update']);
 });
 
